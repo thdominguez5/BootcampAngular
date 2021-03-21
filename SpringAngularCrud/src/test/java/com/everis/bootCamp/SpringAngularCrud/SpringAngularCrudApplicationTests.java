b8eb1ec4f3dac13bc246dc7d19a9223e66a5e99b
tomas_caarrera <thdominguez5@gmail.com>
2021-03-21 12:55:26 +0100

    Nuevo proyecto

---
+++ SpringAngularCrud/src/test/java/com/everis/bootCamp/SpringAngularCrud/SpringAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bootCamp.SpringAngularCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
